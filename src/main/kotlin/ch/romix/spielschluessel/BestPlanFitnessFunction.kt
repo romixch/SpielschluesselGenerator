480d6bd227fe9e2d2da4d5e6627d69f37f3b0141
Roman Schaller <roman.schaller@gmail.com>
2016-10-10 19:27:12 +0200

    Move to own file

--- src/main/kotlin/ch/romix/spielschluessel/BestPlanFitnessFunction.kt
+++ src/main/kotlin/ch/romix/spielschluessel/BestPlanFitnessFunction.kt
@@ -2,9 +2,24 @@ package ch.romix.spielschluessel
 
 import org.jgap.IChromosome
 import org.jgap.FitnessFunction
+import org.jgap.Gene
+import java.util.*
 
-/**
- * Created by roman on 06.10.16.
- */
+class BestPlanFitnessFunction : FitnessFunction {
 
+    constructor()
 
+    override fun evaluate(subject: IChromosome): Double {
+        var fitness: Double = 0.0
+        val genes = subject.genes
+        fitness += calculateGameDiversity(genes)
+        return fitness
+    }
+
+    private fun calculateGameDiversity(genes: Array<Gene>): Double {
+        val distinctGames = HashSet<Game?>()
+        genes.forEach { g -> distinctGames.add((g as GameGene).getGameSlot().game) }
+        return distinctGames.size.toDouble() / genes.size.toDouble()
+    }
+
+}
--- src/main/kotlin/ch/romix/spielschluessel/GeneticDomain.kt
+++ src/main/kotlin/ch/romix/spielschluessel/GeneticDomain.kt
@@ -48,21 +48,3 @@ class GameGene(val gameSlots: ArrayList<GameSlot>, conf: Configuration): BaseGen
     override fun getPersistentRepresentation(): String? = throw UnsupportedOperationException()
 }
 
-class BestPlanFitnessFunction : FitnessFunction {
-
-    constructor()
-
-    override fun evaluate(subject: IChromosome): Double {
-        var fitness: Double = 0.0
-        val genes = subject.genes
-        fitness += calculateGameDiversity(genes)
-        return fitness
-    }
-
-    private fun calculateGameDiversity(genes: Array<Gene>): Double {
-        val distinctGames = HashSet<Game?>()
-        genes.forEach { g -> distinctGames.add((g as GameGene).getGameSlot().game) }
-        return distinctGames.size.toDouble() / genes.size.toDouble()
-    }
-
-}
